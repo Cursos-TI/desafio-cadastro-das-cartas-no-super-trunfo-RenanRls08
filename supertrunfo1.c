b008ced0e8280d50e5b849f8d7b103440756f2fc
RenanRls08 <renanrls.corretor@gmail.com>
2025-02-24 18:05:02 +0000

    Super trunfo cidade 2

---
+++ supertrunfo1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int carta;
+    char codigo[10];
+    char estado[30];
+    char cidade[50];
+    float populacao;
+    float area;
+    float pib;
+    int pturisticos;
+
+    printf("Digite sua carta: \n");
+    scanf("%d", &carta);
+
+    printf("Digite seu código: \n");
+    scanf("%s", codigo);
+
+    printf("Digite seu estado: \n");
+    scanf("%s", estado);
+
+    printf("Digite sua cidade: \n");
+    scanf("%s", cidade);
+
+    printf("Digite sua população: \n");
+    scanf("%f", &populacao);
+
+    printf("Digite a area: \n");
+    scanf("%f", &area);
+
+    printf("Digite seu PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Digite pontos turísticos: \n");
+    scanf("%d", &pturisticos);
+
+    return 0;
+}
